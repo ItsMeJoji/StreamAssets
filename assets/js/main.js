4c8936fee6e0643f0ff70e387858b57bd9ab4dcd
ItsMeJoji <itsmejojitcg@gmail.com>
2025-01-30 23:41:28 -0800

    0.7.5 - Update cuz too many people make it too crowded.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const version = '0.7.4';
+const version = '0.7.5';
 
 $(document).ready(function() {
     $('#footer h6').html(`Version ${version} - Created by <a href="https://itsmejoji.com" target="_blank">ItsMeJoji</a><br/>Images are from the official Pokémon games and are used for non-commercial purposes.`);
